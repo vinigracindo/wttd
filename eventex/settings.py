334c5397d0801791b2ed92ba56843b2e201a004a
Vicente E. R. Marçal <vicente.marcal@gmail.com>
2017-05-23 16:53:32 -0300

    Ajustando o projeto

--- eventex/settings.py
+++ eventex/settings.py
@@ -17,7 +17,7 @@ from dj_database_url import parse as dburl
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
+ 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
 
