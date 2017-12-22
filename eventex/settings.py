15e11835004843092f1dc975b77667e43933de83
Vicente Marçal <vicente.marcal@gmail.com>
2017-12-22 12:35:14 -0200

    Fixed .travis.yml to access PostgreSQL DataBase on tests.
    Close #116

--- eventex/settings.py
+++ eventex/settings.py
@@ -82,9 +82,8 @@ WSGI_APPLICATION = 'eventex.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
-default_dburl = 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')
 DATABASES = {
-    'default': config('DATABASE_URL', default=default_dburl, cast=dburl),
+    'default': config('DATABASE_URL', cast=dburl),
 }
 
 
