830aab644bf3a4b8281b5b3eadb85b465b6e58d0
Vicente Marçal <vicente.marcal@gmail.com>
2018-03-12 11:11:14 -0300

    Completed migration from Django 1.11.11 to 2.0.3

--- eventex/urls.py
+++ eventex/urls.py
@@ -5,8 +5,8 @@ from eventex.core.views import speaker_detail, talk_list, home
 
 urlpatterns = [
     path('', home, name='home'),
-    path('inscricao/', include(('eventex.subscriptions.urls', 'eventex.subscriptions'))),
+    path('inscricao/', include('eventex.subscriptions.urls', namespace='subscriptions')),
     path('palestras/', talk_list, name='talk_list'),
-    path('palestrantes/<int:pk>/', speaker_detail, name='speaker_detail'),
+    path('palestrantes/<slug:slug>/', speaker_detail, name='speaker_detail'),
     path('admin/', admin.site.urls),
 ]
